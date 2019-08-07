ddafed7296b172928fadf13092224caa426b75f2
Levi Miller <paullevimiller@gmail.com>
2019-08-07 12:46:29 -0700

    Added new song view with routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,19 @@ class ApplicationController < Sinatra::Base
     erb :'/songs/index'
   end
 
+  post '/songs' do
+    @artist = Artist.find_or_create_by(name: params[:artist_name])
+    @song = Song.create(params[:song])
+    @song.update(artist: @artist)
+    @confirmation_string = "Successfully created song."
+    redirect to "songs/#{@song.slug}"
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
