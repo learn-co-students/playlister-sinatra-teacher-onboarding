7e4c6db4bd155daa2aff3f6cb7057eb4392184a9
Levi Miller <paullevimiller@gmail.com>
2019-08-07 13:47:00 -0700

    Added basic song edit form view and control, implemented rack flash.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,52 +1,12 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-
+  
   get '/' do
     erb :index
   end
-
-  get '/songs' do
-    @songs = Song.all
-    erb :'/songs/index'
-  end
-
-  post '/songs' do
-    @artist = Artist.find_or_create_by(name: params[:artist_name])
-    @song = Song.create(params[:song])
-    @song.update(artist: @artist)
-    @confirmation_string = "Successfully created song."
-    redirect to "songs/#{@song.slug}"
-  end
-
-  get '/songs/new' do
-    @genres = Genre.all
-    erb :'/songs/new'
-  end
-
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
-  end
-
-  get '/artists' do
-    @artists = Artist.all
-    erb :'/artists/index'
-  end
-
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    erb :'/artists/show'
-  end
-
-  get '/genres' do
-    @genres = Genre.all
-    erb :'/genres/index'
-  end
-
-  get '/genres/:slug' do
-    @genre = Genre.find_by_slug(params[:slug])
-    erb :'/genres/show'
-  end
+  
 end
